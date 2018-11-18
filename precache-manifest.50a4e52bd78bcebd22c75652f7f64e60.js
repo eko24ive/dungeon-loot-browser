1078ccae3588fe0707aeb6c2515e6297fce2a425
Erik Kotsiuba <eko24ive@gmail.com>
2018-11-19 01:56:13 +0200

    Updates

---
+++ precache-manifest.50a4e52bd78bcebd22c75652f7f64e60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76fefd7b2d4edcdc994a",
+    "url": "/dungeon-loot-browser/static/js/runtime~main.76fefd7b.js"
+  },
+  {
+    "revision": "16dd7908eb4284a77546",
+    "url": "/dungeon-loot-browser/static/js/main.16dd7908.chunk.js"
+  },
+  {
+    "revision": "15b8bfcb7f1fabb0ca68",
+    "url": "/dungeon-loot-browser/static/js/1.15b8bfcb.chunk.js"
+  },
+  {
+    "revision": "15b8bfcb7f1fabb0ca68",
+    "url": "/dungeon-loot-browser/static/css/1.7732a87c.chunk.css"
+  },
+  {
+    "revision": "76433148fbe85e1273949ff68f926171",
+    "url": "/dungeon-loot-browser/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d79f459da7ef5d3b750083ea8f617981.js"
+  "/dungeon-loot-browser/precache-manifest.50a4e52bd78bcebd22c75652f7f64e60.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/dungeon-loot-browser/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
