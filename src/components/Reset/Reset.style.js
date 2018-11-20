1e32460adab397cc79feab573471aab53ccd6787
Erik Kotsiuba <eko24ive@gmail.com>
2018-11-20 22:09:30 +0200

    updated reset styles

--- src/components/Reset/Reset.style.js
+++ src/components/Reset/Reset.style.js
@@ -3,11 +3,10 @@ import styled from 'styled-components';
 export default styled.button`
     border: 0;
     width: 100%;
-    padding: 10px 0;
+    padding: 9px 0;
     background: #f2f2f2;
     border-radius: 3px;
     text-transform: uppercase;
-    box-shadow: 1px -1px 1px 0px rgba(0, 0, 0, 0.27);
     font-size: 17px;
     font-family: arial;
     font-weight: bold;
