2a37e024ee117cc3e2860a92348f3f57bd941558
Erik Kotsiuba <eko24ive@gmail.com>
2018-11-23 07:51:19 +0200

    reverse compability fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -16,18 +16,31 @@ import DropZone from './components/DropZone/DropZone';
 import AppWrapper from './elements/AppWrapper/AppWrapper';
 import Title from './elements/Title/Title';
 
+class App extends Component {
+  componentDidCatch() {
+    localStorage.clear();
+    window.location.reload();
+  }
+
+  render() {
+    return (
+      <AppWrapper>
+        <Provider store={store}>
+          <Router>
+            <div>
+              <DropZone />
+              <Title>Dungeon Loot Browser</Title>
+              <Route path="/" exact component={Home} />
+              <Route path="/browse/:dungeon/:item" exact component={Browse} />
+            </div>
+          </Router>
+        </Provider>
+      </AppWrapper>
+    );
+  }
+}
+
 ReactDOM.render(
-  <AppWrapper>
-    <Provider store={store}>
-      <Router>
-        <div>
-          <DropZone />
-          <Title>Dungeon Loot Browser</Title>
-          <Route path="/" exact component={Home} />
-          <Route path="/browse/:dungeon/:item" exact component={Browse} />
-        </div>
-      </Router>
-    </Provider>
-  </AppWrapper>,
+  <App />,
   document.getElementById('root'),
 );
